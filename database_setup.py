ebed7ab96fd8aa2581df9fb13f98790cc6c40e6a
tartufotaruffetti <juan.frachi@gmail.com>
2015-09-21 14:24:45 +0200

    feat: added On delete Cascade
    Added the option to delete on cascade when a parent it is deleted, added some comments and style based on PEP8

--- database_setup.py
+++ database_setup.py
@@ -1,68 +1,79 @@
-'''
-Created on 31/08/2015
-
-@author: juan
-'''
 import os
 import sys
 import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text, LargeBinary
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import String, DateTime, Text, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
+# creating the base
 Base = declarative_base()
 
+
+# creating the User table
 class User(Base):
     __tablename__ = 'user'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(750), nullable=True)
 
+
+# creating the Category table
 class Category(Base):
     __tablename__ = 'category'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
+    # setting the ON DELETE CASCADE
+    category_item = relationship(
+        "CategoryItem",
+        backref="categoria",
+        cascade="all, delete, delete-orphan"
+    )
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
+        """Return object data in easily serializeable format"""
+        return
+        {
+            'name': self.name,
+            'id': self.id
+        }
+
+
+# creating the Items table
 class CategoryItem(Base):
     __tablename__ = 'category_item'
 
-
-    title =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    title = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    category_id = Column(Integer,ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
     image = Column(Text, default="")
-    image_data = Column(LargeBinary, nullable = True)
+    image_data = Column(LargeBinary, nullable=True)
     creation_date = Column(DateTime, default=datetime.datetime.utcnow)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'title'         : self.title,
-           'description'         : self.description,
-           'id'         : self.id
-       }
+        """Return object data in easily serializeable format"""
+        return
+        {
+            'title': self.title,
+            'description': self.description,
+            'id': self.id
+        }
 
-engine = create_engine('sqlite:///catalog.db')
- 
+
+engine = create_engine('sqlite:///catalogitem1.db')
+
 
 Base.metadata.create_all(engine)
